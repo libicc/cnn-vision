b46027db148a5a6bc5455cd5be47d74017881995
libicc <libicc@126.com>
2017-06-12 18:54:43 +0800

    define the multiply gate

--- trained-cnn.py
+++ trained-cnn.py
@@ -1,10 +1,13 @@
 
 
-class MultiGate
-    def forward()
-        
+class MultiplyGate(object)：
+    def forward(x, y)
+        z = x * y
+        self.x = x
+        self.y = y
+        return z
 
-    def backward()
-
-
-
+    def backward(dz)
+    	dx = self.y * dz
+    	dy = self.x * dz
+    	return [dx , dy]
