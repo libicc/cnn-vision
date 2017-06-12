a2881de38b608af3877b6c989b55512f13bb978e
libicc <libicc@126.com>
2017-06-12 19:07:41 +0800

    add three gate and gragh

--- trained-cnn.py
+++ trained-cnn.py
@@ -1,4 +1,21 @@
+class  ComputationalGragh(object):
+	"""docstring for  ComputationalGragh"""
+	def __init__(self, arg):
+		super  ComputationalGragh, self).__init__()
+		self.arg = arg
 
+	def forward(imputs):
+
+		for gate in self.gragh.nodes_topologically_sorted():
+			gate.forwar()
+		return loss
+
+	def backward():
+		for gate in self.gragh.nodes_topologically_sorted():
+			gate.backward()
+			return inputs_gradients
+
+		
 
 class MultiplyGate(object)：
     def forward(x, y)
@@ -11,3 +28,32 @@ class MultiplyGate(object)：
     	dx = self.y * dz
     	dy = self.x * dz
     	return [dx , dy]
+
+
+class AddGate(object)：
+    def forward(x, y)
+        z = x + y
+        self.x = x
+        self.y = y
+        return z
+
+    def backward(dz)
+    	dx =  dz
+    	dy =  dz
+    	return [dx , dy]
+
+class MaxGate(object)：
+    def forward(x, y)
+        z = Mamium(x , y)   
+        self.x = x
+        self.y = y          	
+        return z
+
+    def backward(dz)
+    	if x > y
+    		dx = self.y * dz
+    		dy = 0
+    	else 
+    		dy = self.x *dz
+    		dx = 0
+      	return [dx , dy]
